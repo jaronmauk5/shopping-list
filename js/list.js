b11124ece730442a47e73e43f2c223844982f7cd
Jaron Mauk <jaronmauk5@gmail.com>
2016-05-18 16:38:39 -0700

    Added feature to create and change between lists

--- js/list.js
+++ js/list.js
@@ -9,15 +9,45 @@ $(document).ready(function(){
 		}
 	});
 
+		//prompting user for list name
+	$('.fa-plus-square-o').on('click', function () {
+			var name = prompt('What do you want to call this list?');
+			if (name) {
+				addList(name);
+			}
+	});
+		
+	$('.list-menu').on('click', 'a', function () {
+			var listId = $(this).attr('data-list');
+			$('.lists').find('[data-list]').hide();
+			var $list = $('.lists').find('[data-list=' + listId + ']');
+			$list.show();
+	});
+
 
 	$.fn.todoList = function(){
 		return this.each(function(){
 
 			var defaults = {
+				title: 'To Do',
+				template: '<form class="form-group">\
+						<h2>{title}</h2>\
+							<hr>\
+							<div class="submit-wrapper">\
+							<input type="text" name="list-input" class="input-text" placeholder="Add something!">\
+								<button id="submit-button">Add item to list</button>\
+							</div>\
+							</form>\
+							<ul class="list">\
+							<!-- check box and list items -->\
+									</ul>'
 
 			};
 
+			var config = $.extend({},default, options);
+
 			var $el = $(this);
+			$el.html(config.template.replace('{title}', config.title));
 
 			$el.find('.form-group').submit(function(event){
 				event.preventDefault();
@@ -33,13 +63,29 @@ $(document).ready(function(){
 			});
 
 			$el.find('.list').on('click', '.fa-trash-o', function(event){
-				$(event.currentTarget).parents("li").remove();
+					$(event.currentTarget).parents("li").remove();
 			});
 		});	
 
 	};
 
+	function addList(name) {
+		var listId = $('.lists').children().length;
+		var $el = $('<div>', { class: 'item-list', 'data-list': listId});
+		$el.todoList({
+			title:name
+		});
+		var $menuItem = $('<li>').append(
+			$('<a href="#")' + name + '</a>').attr('data-list', listId)
+		);
+		$('.list-menu ul').append($menuItem);
+		$('.lists').append($el);
+		return $el;
+	}
+
 	//initialize
-	$('.item-list').todoList();
+	addList('Shopping List');
+
+	window.addList = addList;
 
 }); //doc.ready

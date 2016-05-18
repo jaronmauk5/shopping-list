6562a54c458cf06aa73c5548b18f95ab55553885
Jaron Mauk <jaronmauk5@gmail.com>
2016-05-18 12:14:44 -0700

    added animation for side bar menu

--- js/list.js
+++ js/list.js
@@ -1,19 +1,45 @@
 $(document).ready(function(){
-	$('.form-group').submit(function(event){
-		event.preventDefault();
-		var something = $('.input-text').val();
-	// if user inputs nothing and hits enter, alert will pop up and nothing is appended
-		if (something === "") {
-			alert('Please enter an item!')
+	$('.fa-bars').on('click',function() {
+		if($('.list-menu').css('left')=='0px') {
+			$('.list-menu').animate({left:'-100%'});
 		}
-		else {
-			$('.list').append("<li class ='currentItem'><label class='checkmark'><input type='checkbox' class='is-completed'><span class='check-icon'></span><span class= 'item-name'>" + " " + something + " " +  "</span></label><i class='fa fa-trash-o' aria-hidden='true'></i></li>")
-			$('.input-text').val("");
+		else {	
+
+			$('.list-menu').animate({left:0});
 		}
 	});
 
-	$('.list').on('click', '.fa-trash-o', function(event){
-		$(event.currentTarget).parents("li").remove();
-	});
+
+	$.fn.todoList = function(){
+		return this.each(function(){
+
+			var defaults = {
+
+			};
+
+			var $el = $(this);
+
+			$el.find('.form-group').submit(function(event){
+				event.preventDefault();
+				var something = $el.find('.input-text').val();
+				// if user inputs nothing and hits enter, alert will pop up and nothing is appended
+				if (something === "") {
+				alert('Please enter an item!')
+				}
+				else {
+					$el.find('.list').append("<li class ='currentItem'><label class='checkmark'><input type='checkbox' class='is-completed'><span class='check-icon'></span><span class= 'item-name'>" + " " + something + " " +  "</span></label><i class='fa fa-trash-o' aria-hidden='true'></i></li>")
+					$el.find('.input-text').val("");
+				}
+			});
+
+			$el.find('.list').on('click', '.fa-trash-o', function(event){
+				$(event.currentTarget).parents("li").remove();
+			});
+		});	
+
+	};
+
+	//initialize
+	$('.item-list').todoList();
 
 }); //doc.ready

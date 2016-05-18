f0da1a009c8cc82f0c23ca1cb52deb7c6e211508
chad d <chad@cmfolio.com>
2016-05-18 13:50:13 -0600

    Sample adding list

--- js/list.js
+++ js/list.js
@@ -8,16 +8,46 @@ $(document).ready(function(){
 			$('.list-menu').animate({left:0});
 		}
 	});
+	
+	$('.fa-plus-square-o').on('click', function () {
+		var name = prompt('What do you want to call this list?');
+		if (name) {
+			addList(name);
+		}
+	});
+	
+	$('.list-menu').on('click', 'a', function () {
+		var listId = $(this).attr('data-list');
+		$('.lists').find('[data-list]').hide();
+		var $list = $('.lists').find('[data-list=' + listId + ']');
+		$list.show();
+	});
+	
+	
 
 
-	$.fn.todoList = function(){
+	$.fn.todoList = function(options){
 		return this.each(function(){
 
 			var defaults = {
-
+				title: 'To Do',
+				template: '<form class="form-group">\
+										<h2>{title}</h2>\
+										<hr>\
+										<div class="submit-wrapper">\
+											<input type="text" name="list-input" class="input-text" placeholder="Add something!">\
+											<button id="submit-button">Add item to list</button>\
+										</div>\
+									</form>\
+									<ul class="list">\
+										<!-- check box and list items -->\
+									</ul>'
 			};
 
+			var config = $.extend({}, defaults, options);
+			
 			var $el = $(this);
+			$el.html(config.template.replace('{title}', config.title));
 
 			$el.find('.form-group').submit(function(event){
 				event.preventDefault();
@@ -38,8 +68,24 @@ $(document).ready(function(){
 		});	
 
 	};
+	
+	function addList(name) {
+		var listId = $('.lists').children().length;
+		var $el = $('<div>', { class: 'item-list', 'data-list': listId });
+		$el.todoList({
+			title: name
+		});
+		var $menuItem = $('<li>').append(
+			$('<a href="#">' + name + '</a>').attr('data-list', listId)
+		);
+		$('.list-menu ul').append($menuItem);
+		$('.lists').append($el);
+		return $el;
+	}
 
 	//initialize
-	$('.item-list').todoList();
+	addList('Shopping List');
+	
+	window.addList = addList;
 
 }); //doc.ready
